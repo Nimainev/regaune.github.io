602e7b080af07833cb722734152e76ad6d5ed700
Nimainev <111495433+Nimainev@users.noreply.github.com>
2025-01-19 03:59:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,279 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/graph-tabs/src/graph-tabs.js":
+/*!***************************************************!*\
+  !*** ./node_modules/graph-tabs/src/graph-tabs.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ GraphTabs)
+/* harmony export */ });
+class GraphTabs {
+  constructor(selector, options) {
+    let defaultOptions = {
+      isChanged: () => {}
+    }
+    this.options = Object.assign(defaultOptions, options);
+    this.selector = selector;
+    this.tabs = document.querySelector(`[data-tabs="${selector}"]`);
+    if (this.tabs) {
+      this.tabList = this.tabs.querySelector('.tabs__nav');
+      this.tabsBtns = this.tabList.querySelectorAll('.tabs__nav-btn');
+      this.tabsPanels = this.tabs.querySelectorAll('.tabs__panel');
+    } else {
+      console.error('Селектор data-tabs не существует!');
+      return;
+    }
+
+    this.check();
+    this.init();
+    this.events();
+  }
+
+  check() {
+    if (document.querySelectorAll(`[data-tabs="${this.selector}"]`).length > 1) {
+      console.error('Количество элементов с одинаковым data-tabs больше одного!');
+      return;
+    }
+
+    if (this.tabsBtns.length !== this.tabsPanels.length) {
+      console.error('Количество кнопок и элементов табов не совпадает!');
+      return;
+    }
+  }
+
+  init() {
+    this.tabList.setAttribute('role', 'tablist');
+
+    this.tabsBtns.forEach((el, i) => {
+      el.setAttribute('role', 'tab');
+      el.setAttribute('tabindex', '-1');
+      el.setAttribute('id', `${this.selector}${i + 1}`);
+      el.classList.remove('tabs__nav-btn--active');
+    });
+
+    this.tabsPanels.forEach((el, i) => {
+      el.setAttribute('role', 'tabpanel');
+      el.setAttribute('tabindex', '-1');
+      el.setAttribute('aria-labelledby', this.tabsBtns[i].id);
+      el.classList.remove('tabs__panel--active');
+    });
+
+    this.tabsBtns[0].classList.add('tabs__nav-btn--active');
+    this.tabsBtns[0].removeAttribute('tabindex');
+    this.tabsBtns[0].setAttribute('aria-selected', 'true');
+    this.tabsPanels[0].classList.add('tabs__panel--active');
+  }
+
+  events() {
+    this.tabsBtns.forEach((el, i) => {
+      el.addEventListener('click', (e) => {
+        let currentTab = this.tabList.querySelector('[aria-selected]');
+
+        if (e.currentTarget !== currentTab) {
+          this.switchTabs(e.currentTarget, currentTab);
+        }
+      });
+
+      el.addEventListener('keydown', (e) => {
+        let index = Array.prototype.indexOf.call(this.tabsBtns, e.currentTarget);
+
+        let dir = null;
+
+        if (e.which === 37) {
+          dir = index - 1;
+        } else if (e.which === 39) {
+          dir = index + 1;
+        } else if (e.which === 40) {
+          dir = 'down';
+        } else {
+          dir = null;
+        }
+
+        if (dir !== null) {
+          if (dir === 'down') {
+            this.tabsPanels[i].focus();
+          } else if (this.tabsBtns[dir]) {
+            this.switchTabs(this.tabsBtns[dir], e.currentTarget);
+          }
+        }
+      });
+    });
+  }
+
+  switchTabs(newTab, oldTab = this.tabs.querySelector('[aria-selected]')) {
+    newTab.focus();
+    newTab.removeAttribute('tabindex');
+    newTab.setAttribute('aria-selected', 'true');
+
+    oldTab.removeAttribute('aria-selected');
+    oldTab.setAttribute('tabindex', '-1');
+
+    let index = Array.prototype.indexOf.call(this.tabsBtns, newTab);
+    let oldIndex = Array.prototype.indexOf.call(this.tabsBtns, oldTab);
+
+    this.tabsPanels[oldIndex].classList.remove('tabs__panel--active');
+    this.tabsPanels[index].classList.add('tabs__panel--active');
+
+    this.tabsBtns[oldIndex].classList.remove('tabs__nav-btn--active');
+    this.tabsBtns[index].classList.add('tabs__nav-btn--active');
+
+    this.options.isChanged(this);
+  }
+}
+
+/***/ }),
+
+/***/ "./src/js/_components.js":
+/*!*******************************!*\
+  !*** ./src/js/_components.js ***!
+  \*******************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+console.log('components');
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_components.js */ "./src/js/_components.js");
+/* harmony import */ var graph_tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! graph-tabs */ "./node_modules/graph-tabs/src/graph-tabs.js");
+
+
+// Инициализация миниатюр
+const thumbsSwiper = new Swiper('.swiper-thumbs', {
+  direction: 'vertical',
+  // Вертикальное расположение миниатюр
+  slidesPerView: 3,
+  // Количество миниатюр
+  spaceBetween: 10,
+  // Отступы между миниатюрами
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true
+});
+
+// Инициализация основного слайдера
+const mainSwiper = new Swiper('.main-slider', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  thumbs: {
+    swiper: thumbsSwiper // Привязка миниатюр
+  }
+});
+
+// Инициализация миниатюр
+const secondThumbs = new Swiper('.second-thumbs', {
+  direction: 'vertical',
+  // Вертикальное расположение миниатюр
+  slidesPerView: 3,
+  // Количество миниатюр
+  spaceBetween: 10,
+  // Отступы между миниатюрами
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true
+});
+
+// Инициализация основного слайдера
+const secondSwiper = new Swiper('.second-slider', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  thumbs: {
+    swiper: secondThumbs // Привязка миниатюр
+  }
+});
+var swiper = new Swiper(".myFirst", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+const tabs = new graph_tabs__WEBPACK_IMPORTED_MODULE_1__["default"]('tab');
+
+// var swiper = new Swiper(".myOne", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
